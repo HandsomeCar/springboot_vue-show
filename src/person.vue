d13aa6de51d98fec7a4bee25b8088e2a740c4acf
car <jiaqi@ingdee.com>
2023-07-14 16:35:44 +0800

    初始化

---
+++ src/person.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>编号</td>
+        <td>名字</td>
+        <td>性别</td>
+      </tr>
+      <tr v-for="person in personEnt" :key="person">
+        <td>{{person.id}}</td>
+        <td>{{person.name}}</td>
+        <td>{{person.sex}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Person",
+  data(){
+    return {
+      personEnt:[
+        {
+          id: 1,
+          name: '张三',
+          sex: '男'
+        },
+        {
+          id: 2,
+          name: '李四',
+          sex: '女'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
