b7671359fc057401d7b9c4fb405d6b16132ddd29
car <jiaqi@ingdee.com>
2023-07-18 14:30:23 +0800

    前端初始化

--- src/main.js
+++ src/main.js
@@ -3,29 +3,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './person/router/router'
 import store from '/store/index.js'
-import {
-    components,
-    plugins
-} from './common/element'
+import ElementPlus from '../node_modules/element-plus';
 
 // 新增代码：引入全部组件及样式
-import 'element-plus/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
-components.forEach(component => {
-    app.component(component.name, component)
-})
-
-plugins.forEach(plugin => {
-    app.use(plugin)
-})
-
 app.use(store)
 app.use(router)
-// 新增代码：注册特定组件
-// app.component(ElButton.name, ElButton)
-// 新增代码：注册全部组件
-// app.use(ElementPlus)
-
+app.use(ElementPlus)
 app.mount('#app')
 
