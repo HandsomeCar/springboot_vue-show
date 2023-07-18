282c70455cd78c96adfcbac450f311f3a3b552d1
car <jiaqi@ingdee.com>
2023-07-18 18:05:00 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,51 @@
 <template>
-  <div>
-    <table>
-      <tr>
-        <td>编号</td>
-        <td>名字</td>
-        <td>性别</td>
-      </tr>
-      <tr v-for="App in personEnt" :key="App">
-        <td>{{ App.id }}</td>
-        <td>{{ App.name }}</td>
-        <td>{{ App.sex }}</td>
-      </tr>
-    </table>
-  </div>
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+    <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+    <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+    <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+  </el-tabs>
 
-  <div class="hello">
-    <h1>{{ "666666666666"}}</h1>
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="1">处理中心</el-menu-item>
+    <el-submenu index="2">
+      <template #title>我的工作台</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+      <el-submenu index="2-4">
+        <template #title>选项4</template>
+        <el-menu-item index="2-4-1">选项1</el-menu-item>
+        <el-menu-item index="2-4-2">选项2</el-menu-item>
+        <el-menu-item index="2-4-3">选项3</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-menu-item index="3" disabled>消息中心</el-menu-item>
+    <el-menu-item index="4"><a href="https://www.baidu.com" target="_blank">订单管理</a></el-menu-item>
+    <el-menu-item index="3" disabled>消息中心</el-menu-item>
+    <el-menu-item index="4"><a href="https://www.baidu.com" target="_blank">订单管理</a></el-menu-item>
+  </el-menu>
 
-    <el-row>
-      <el-button plain>朴素按钮</el-button>
-      <el-button type="primary" plain>主要按钮</el-button>
-      <el-button type="success" plain>成功按钮</el-button>
-      <el-button type="info" plain>信息按钮</el-button>
-      <el-button type="warning" plain>警告按钮</el-button>
-      <el-button type="danger" plain>危险按钮</el-button>
-    </el-row>
 
-    <el-row>
-      <el-button round>圆角按钮</el-button>
-      <el-button type="primary" round>主要按钮</el-button>
-      <el-button type="success" round>成功按钮</el-button>
-      <el-button type="info" round>信息按钮</el-button>
-      <el-button type="warning" round>警告按钮</el-button>
-      <el-button type="danger" round>危险按钮</el-button>
-    </el-row>
-
-    <el-row>
-      <el-button icon="el-icon-search" circle></el-button>
-      <el-button type="primary" icon="el-icon-edit" circle></el-button>
-      <el-button type="success" icon="el-icon-check" circle></el-button>
-      <el-button type="info" icon="el-icon-message" circle></el-button>
-      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-      <el-button type="danger" icon="el-icon-delete" circle></el-button>
-    </el-row>
-  </div>
 </template>
 
-
-
-
 <script>
-import axios from "axios";
-
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: "App",
-  data(){
+  data() {
     return {
-      personEnt:[
-        {
-          id: 1,
-          name: '张三',
-          sex: '男'
-        },
-        {
-          id: 2,
-          name: '李四',
-          sex: '女'
-        }
-      ]
-    }
+      activeIndex: '1',
+      activeIndex2: '1',
+      activeName: 'second'
+    };
   },
-  created() {
-    var that=this;
-    axios.get('http://localhost:8081/person/selectAll').then(function (resp) {
-      that.personEnt=resp.data;
-    })
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
   }
-
 }
 </script>
 
